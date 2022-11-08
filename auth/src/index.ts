12c296bcf7a1bad50b447ea3c2676f92ccf76f99
yuhariermy <yuhariermy@gmail.com>
2022-11-08 23:59:32 +0700

    initial commit

---
+++ auth/src/index.ts
@@ -0,0 +1,11 @@
+import express from "express"
+import {json} from "body-parser"
+
+const app = express()
+
+app.use(json())
+
+
+app.listen(3000, () => {
+    console.log("Server listening on port 3000")
+})
