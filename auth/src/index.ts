5d8dbaf440901be7e448f3568b6abb717acda4b8
yuhariermy <yuhariermy@gmail.com>
2022-11-09 00:32:12 +0700

    initial commit with skaffold, ingress nginx and k8s

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,8 +2,11 @@ import express from "express"
 import {json} from "body-parser"
 
 const app = express()
-
 app.use(json())
+
+app.get('/api/users/currentuser', (req, res) => { 
+    res.send('Hi There')
+})
 
 
 app.listen(3000, () => {
