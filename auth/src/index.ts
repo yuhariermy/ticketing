057c910c95e97e11360431ffa0338202d4df66ea
yuhariermy <yuhariermy@gmail.com>
2022-11-09 20:35:09 +0700

    finishing Error handler

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import 'express-async-errors'
 import { json } from "body-parser";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/singup";
+import { NotFoundError } from "./errors/not-found-error";
 
 import { errorHandler } from "./middlewares/error-handler";
 
@@ -15,6 +17,10 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+})
 
 app.use(errorHandler)
 
--- auth/src/routes/singup.ts
+++ auth/src/routes/singup.ts
@@ -10,7 +10,7 @@ const router = express.Router()
 router.post('/api/users/signup', [
     body('email').isEmail().withMessage('Email mush be valid'),
     body('password').trim().isLength({ min: 4, max: 20 }).withMessage('Password mus be between 4 and 20 characters')
-], (req: Request, res: Response) => {
+], async (req: Request, res: Response) => {
     const errors = validationResult(req)
 
     if (!errors.isEmpty()) {
