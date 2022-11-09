ae040e938ce20d8c8a969bc24081d02d5adf5dae
yuhariermy <yuhariermy@gmail.com>
2022-11-09 21:25:48 +0700

    connecting mongodb on kubernetes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,14 +1,14 @@
 import express from "express";
 import 'express-async-errors'
 import { json } from "body-parser";
+import mongoose from "mongoose"
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/singup";
-import { NotFoundError } from "./errors/not-found-error";
-
 import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
@@ -24,7 +24,23 @@ app.all('*', async (req, res) => {
 
 app.use(errorHandler)
 
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
+      // useCreateIndex: true
+    });
+    console.log('Connected to MongoDb');
+  } catch (err) {
+    console.error(err);
+  }
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
-});
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!!!!!!!');
+  });
+}
+
+start();
+
+
