c378ca7ad798e5e75c1cd82a136812eb22fd66cf
yuhariermy <yuhariermy@gmail.com>
2022-11-09 21:27:22 +0700

    fixing connecting mongo in kubernetes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -26,11 +26,7 @@ app.use(errorHandler)
 
 const start = async () => {
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
-      // useNewUrlParser: true,
-      // useUnifiedTopology: true,
-      // useCreateIndex: true
-    });
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
     console.log('Connected to MongoDb');
   } catch (err) {
     console.error(err);
