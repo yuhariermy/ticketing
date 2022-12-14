dbccb1ec67897cbbada5cb6d96c116b5600282e3
yuhariermy <yuhariermy@gmail.com>
2022-12-14 14:52:10 +0700

    configure webpack on next.config.js and  _app.js

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a7a35df4c00f9eb9"; }
+/******/ 		__webpack_require__.h = function() { return "f281a76f50478c26"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
@@ -1222,6 +1222,11 @@
 /******/ 		var chunkLoadingGlobal = self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/nonce */
+/******/ 	!function() {
+/******/ 		__webpack_require__.nc = undefined;
 /******/ 	}();
 /******/ 	
 /************************************************************************/
--- client/pages/banana.js
+++ client/pages/banana.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const banana = () => {
-  return <h1>banana</h1>;
+  return <h1>banana2</h1>;
 };
 
 export default banana;
